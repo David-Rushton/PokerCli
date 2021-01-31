13f2a45a39c59a03f611ecd831451fe932497376
David Rushton <david-rushton@outlook.com>
2021-01-31 22:59:04 +0000

    Deck supports refactored Card

--- src/PokerCli/Model/Deck.cs
+++ src/PokerCli/Model/Deck.cs
@@ -14,13 +14,16 @@ namespace PokerCli.Model
         public int RemainingCards => _deck.Count;
 
 
-        public IEnumerable<Card> Deal(int numberOfCards)
+        public IEnumerable<Card> Deal(int cardsRequested)
         {
-            for(var i = 0; i < numberOfCards; i++)
+            if(cardsRequested > RemainingCards)
+                throw new Exception($"Cannot deal {cardsRequested} card(s).  Deck contains {RemainingCards} card(s).");
+
+            for(var i = 0; i < cardsRequested; i++)
                 yield return NextCard();
         }
 
-        public void Shuffle() => RefillDeck();
+        public Deck Shuffle() => RefillDeck();
 
 
         private Card NextCard()
@@ -37,12 +40,12 @@ namespace PokerCli.Model
         }
 
         // TODO: Why rebuild this time and time again?
-        private void RefillDeck()
+        private Deck RefillDeck()
         {
             _deck.Clear();
 
             for(var i = 0; i < 52; i++)
-                _deck.Add(new Card(GetSuit(i), GetRank(i)));
+                _deck.Add(new Card(GetCardRank(i), GetSuit(i)));
 
 
             CardSuit GetSuit(int idx) =>
@@ -52,20 +55,40 @@ namespace PokerCli.Model
                     1 => CardSuit.Spades,
                     2 => CardSuit.Diamonds,
                     3 => CardSuit.Clubs,
-                    _ => throw new Exception("Cannot generator card suit.  Unexpected seed value")
+
+                    _ => throw new Exception("Cannot generator card suit.  Unexpected seed value.")
                 }
             ;
 
-            CardRank GetRank(int idx)
+
+            Debug.Assert(_deck.Count == 52, $"After a shuffle the deck should contain 52 cards, not {_deck.Count}.");
+            return this;
+
+
+            CardRank GetCardRank(int idx) =>
+                new CardRank(GetCardRankSymbol(idx))
+            ;
+
+            CardRankSymbol GetCardRankSymbol(int idx)
             {
                 var rankValue = ((idx % 13) + 2);
                 return (rankValue) switch
                 {
-                    11 => CardRank.Jack,
-                    12 => CardRank.Queen,
-                    13 => CardRank.King,
-                    14 => CardRank.Ace,
-                    _  => (CardRank)rankValue
+                     2 => CardRankSymbol.Two,
+                     3 => CardRankSymbol.Three,
+                     4 => CardRankSymbol.Four,
+                     5 => CardRankSymbol.Five,
+                     6 => CardRankSymbol.Six,
+                     7 => CardRankSymbol.Seven,
+                     8 => CardRankSymbol.Eight,
+                     9 => CardRankSymbol.Nine,
+                    10 => CardRankSymbol.Ten,
+                    11 => CardRankSymbol.Jack,
+                    12 => CardRankSymbol.Queen,
+                    13 => CardRankSymbol.King,
+                    14 => CardRankSymbol.Ace,
+
+                    _ => throw new Exception("Cannot generator card rank.  Unexpected seed value")
                 };
             }
         }
