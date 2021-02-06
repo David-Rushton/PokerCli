2b10b33f5ccabb2806c68b71e12e3df90d9db913
David Rushton <david-rushton@outlook.com>
2021-02-06 23:00:07 +0000

    Adds Croupier to manage betting WIP

---
+++ src/PokerCli/Config/CroupierConfig.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+
+namespace PokerCli.Config
+{
+    public record CroupierConfig(decimal SmallBlind, decimal BigBlind);
+}
